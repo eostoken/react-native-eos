7f79710eca2dcd231f0e9378161b28f318cae57b
william <william@dacrs.com>
2018-05-26 22:28:03 +0800

    add verify privatekey

--- src/eosapi.js
+++ src/eosapi.js
@@ -3,5 +3,6 @@ export default {
     eos_sign_data:"eos_sign_data",
     eos_transfer:"eos_transfer",
     eos_create_account:"eos_create_account",
-    eos_balance:"eos_balance"
+    eos_balance:"eos_balance",
+    eos_verify_pk:"eos_verify_pk"
 }
